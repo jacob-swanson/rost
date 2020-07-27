480277e23848fba7b27f6ddec367ad762a3b4a33
Jacob Swanson <jacob.swanson.n@gmail.com>
2020-07-27 18:41:45 -0500

    Create a freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // disable the Rust standard library
+#![no_main] // disable the Rust entry point
+
+use core::panic::PanicInfo;
+
+/// Automatically called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// System entry point.
+#[no_mangle] // output non-cryptic function name
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
