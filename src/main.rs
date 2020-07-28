4ded603b0fc3537bd10588958c7fa829f1a2ee9f
Jacob Swanson <jacob.swanson.n@gmail.com>
2020-07-27 20:54:16 -0500

    Achieve first boot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // disable the Rust standard library
 #![no_main] // disable the Rust entry point
 
+extern crate rlibc;
+
 use core::panic::PanicInfo;
 
 /// Automatically called on panic.
@@ -10,7 +12,17 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 /// System entry point.
+/// Must be named _start by convention.
 #[no_mangle] // output non-cryptic function name
 pub extern "C" fn _start() -> ! {
+    // Write "Hello World!" to the framebuffer.
+    // By convention the framebuffer is at this address in RAM.
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in b"Hello World!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
